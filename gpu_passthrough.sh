aca273c0f23f8a899b04275cee422c5fc63e2172
pavolelsig <52982339+pavolelsig@users.noreply.github.com>
2019-07-17 22:12:36 -0700

    Add files via upload

--- gpu_passthrough.sh
+++ gpu_passthrough.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-dnf update
+#dnf update
 
-dnf install qemu qemu-img nano
+echo "Installing required packages"
 
-dnf groupinstall "Virtualization"
+dnf install qemu qemu-img nano -y
+
+dnf groupinstall "Virtualization" -y
+
+echo "Egit grub: intel_iommu=on or amd_iommu=on rd.driver.pre=vfio-pci kvm.ignore_msrs=1"
 
 nano /etc/sysconfig/grub
 
+echo "Updating grub"
+
 grub2-mkconfig -o /etc/grub2-efi.cfg
+
+echo "Getting GPU passthrough scripts ready"
 
 cp vfio-pci-override-vga.sh /sbin/vfio-pci-override-vga.sh
 
@@ -17,5 +25,7 @@ chmod 755 /sbin/vfio-pci-override-vga.sh
 echo "install vfio-pci /sbin/vfio-pci-override-vga.sh" > /etc/modprobe.d/local.conf
 
 cp local.conf /etc/dracut.conf.d/local.conf
+
+echo "Generating initramfs"
 
 dracut -f --kver `uname -r`
